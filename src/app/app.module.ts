42b36ff0e614225ce50fe811344786d536cbeb10
Giulio <ggiudice94@gmail.com>
2022-02-02 15:14:13 +0100

    modifiche left component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserContainerComponent } from './user-container/user-container.component';
 import { OrderContainerComponent } from './order-container/order-container.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserContainerComponent,
-    OrderContainerComponent
+    OrderContainerComponent,
+    UserListComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule
